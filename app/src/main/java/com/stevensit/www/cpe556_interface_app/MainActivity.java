1ffe0305321c9cf4bffdaefd3822c78600842db5
Fady Safar <fsafar@stevens.edu>
2018-11-10 16:30:26 -0500

    interface is ready, didnt start with the code yet

---
+++ app/src/main/java/com/stevensit/www/cpe556_interface_app/MainActivity.java
@@ -0,0 +1,15 @@
+package com.stevensit.www.cpe556_interface_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
